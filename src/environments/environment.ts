f4509df567c5db0b596d98e37fdc1b8221afc3a4
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-07-29 21:05:28 +0200

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'kobulovmytodolist',
+    appId: '1:83617179338:web:67c56fbc320c0e15897633',
+    storageBucket: 'kobulovmytodolist.appspot.com',
+    apiKey: 'AIzaSyAbHo4qCoN4HsRRppalV0h2IoVDJbGDAkQ',
+    authDomain: 'kobulovmytodolist.firebaseapp.com',
+    messagingSenderId: '83617179338',
+    measurementId: 'G-FTVT1R0DVM',
+  },
+};
