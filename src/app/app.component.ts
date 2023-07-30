eaa2473b27baac8f01a26b2a6d3c67d259eed598
Ulugbek Kobulov <108155304+KobulovUlugbek@users.noreply.github.com>
2023-07-30 16:22:00 +0200

    the end create todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+  todos$: Observable<any>;
+  firestore: Firestore = inject(Firestore);
+  todos:Array<any>;
+  todotext = '';
+
+  constructor() {
+    const itemCollection = collection(this.firestore, 'todos');
+    this.todos$ = collectionData(itemCollection);
+
+    this.todos$.subscribe( (newTodos) => {
+      console.log('Todos updated', newTodos)
+      this.todos = newTodos;
+    });
+  }
+
+  addTodo(): void{
+    const itemCollection = collection(this.firestore, 'todos');
+    setDoc(doc(itemCollection), {name: this.todotext});
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
